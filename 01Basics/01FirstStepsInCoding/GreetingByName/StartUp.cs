4d851c872afcac33e6dd96b36fde635fbb52461a
NIKONaaaaa <vintageblacksea@gmail.com>
2021-04-14 23:41:48 +0300

    update

---
+++ 01Basics/01FirstStepsInCoding/GreetingByName/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace GreetingByName
+{
+    using System;
+    class StartUp
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
