e420c421aaeceb71e548ac1653e6505a427ca4b6
NIKONaaaaa <vintageblacksea@gmail.com>
2021-06-08 20:45:47 +0300

    Interface Lab Task 1

---
+++ 04OOP/03InterfacesAndAbstractionLab/Shapes/Circle.cs
@@ -0,0 +1,42 @@
+﻿namespace Shapes
+{
+    using System;
+    public class Circle : IDrawable
+    {
+
+        private int radius;
+
+        public int Radius
+        {
+            get => radius;
+            private set => radius = value;
+        }
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public void Draw()
+        {
+            double rIn = this.radius - 0.4;
+            double rOut = this.radius + 0.4;
+            for (double y = this.radius; y >= -this.radius; --y)
+            {
+                for (double x = -this.radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 04OOP/03InterfacesAndAbstractionLab/Shapes/Rectangle.cs
@@ -0,0 +1,47 @@
+﻿namespace Shapes
+{
+    using System;
+    public class Rectangle : IDrawable
+    {
+        private int height;
+        private int width;
+
+        public Rectangle(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public int Height
+        {
+            get => height;
+            private set => height = value;
+        }
+
+        public int Width
+        {
+            get => width;
+            private set => width = value;
+        }
+
+        public void Draw()
+        {
+            DrawLine(this.width, '*', '*');
+            for (int i = 1; i < this.height - 1; ++i)
+            {
+                DrawLine(this.width, '*', ' ');
+            }
+            DrawLine(this.width, '*', '*');
+        }
+
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width; i++)
+            {
+                Console.Write(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+}
