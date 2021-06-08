a7d17aa46c78daf72bf6b754d6a70ee2a81ee905
NIKONaaaaa <vintageblacksea@gmail.com>
2021-06-08 21:03:46 +0300

    Task 2

---
+++ 04OOP/03InterfacesAndAbstractionLab/Cars/Tesla.cs
@@ -0,0 +1,31 @@
+﻿namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; private set; }
+        public string Color { get; private set; }
+        public int Batteries { get; private set; }
+
+        public Tesla(string model, string color, int batteries)
+        {
+            Model = model;
+            Color = color;
+            Batteries = batteries;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} {Model} with {Batteries} Batteries\n{Start()}\n{Stop()}";
+        }
+    }
+}
