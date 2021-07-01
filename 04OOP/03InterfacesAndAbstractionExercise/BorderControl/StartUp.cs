a1d90b71b1e0c0d0f4565fe8aa312f0f27dac65a
NIKONaaaaa <vintageblacksea@gmail.com>
2021-07-01 15:27:25 +0300

    Interfaces Task 4

---
+++ 04OOP/03InterfacesAndAbstractionExercise/BorderControl/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace BorderControl
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class StartUp
+    {
+        public static void Main()
+        {
+            string command;
+            List<IIdentifiable> entries = new List<IIdentifiable>();
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] entity = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (entity.Length == 2)
+                {
+                    entries.Add(new Robot(entity[0], entity[1]));
+                }
+                else if (entity.Length == 3)
+                {
+                    entries.Add(new Citizen(entity[0], int.Parse(entity[1]), entity[2]));
+                }
+            }
+
+            string checkId = Console.ReadLine();
+
+            foreach (var entry in entries.Where(entry => entry.Id.EndsWith(checkId)))
+            {
+                Console.WriteLine(entry.Id);
+            }
+        }
+    }
+}
