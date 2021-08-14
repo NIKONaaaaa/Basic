668256a4c7d4cb305b5f8e92e1f9c9448f97be60
NIKONaaaaa <vintageblacksea@gmail.com>
2021-08-14 22:41:17 +0300

    twer
    wert

---
+++ 04OOP/08ReflectionAndAttributesLab/AuthorProblem/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthorProblem
+{
+    using System;
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
